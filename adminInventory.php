5a6089481245c7cd655bacac14b1c24e49a639c9
buntman <148082762+buntman@users.noreply.github.com>
2025-01-19 14:37:54 +0800

    added inventory ui

---
+++ adminInventory.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/admin-inventory-style.css">
+<title>Inventory</title>
+</head>
+    <body>
+        <div class="sidenav">
+            <a href="#">Inventory</a>
+            <a href="#">Reports</a>
+            <a href="#">Manage</a>
+        </div>
+        <div class="main-container">
+            <div class="order">
+            </div>
+            <div class="stocks">
+                <div class="card-title">
+                    <h2>Current Stocks</h2>
+                    <div class="button-container">
+                        <button type="submit" name="add" value="add">Add New Products</button>
+                    </div>
+                </div>
+                <div class="current-stocks">
+                </div>
+            </div>
+        </div>
+</body>
+</html> 
