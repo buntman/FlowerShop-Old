e99aa0b28ac6b9bf2c7e08647c62b82b805b6201
buntman <148082762+buntman@users.noreply.github.com>
2025-03-09 16:02:35 +0800

    combined into one file

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -30,6 +30,35 @@ function sendProductDetails(product_name) {
     .catch(error=> console.error('Error', error));
 }
 
+const delete_button = document.getElementById('delete-button');
+const name = document.getElementById('product-name');
+
+delete_button.addEventListener("click", () => {
+    const product_name = name.textContent;
+    sendNameToServer(product_name);
+});
+
+function sendNameToServer(product_name) {
+    fetch('/delete', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({product_name})
+    })
+    .then(response=>response.json())
+    .then(data=> {
+            if(data.success) {
+                document.getElementById('product-image').remove();
+                document.getElementById('product-name').remove();
+                document.getElementById('product-description').remove();
+                document.getElementById('product-price').remove();
+            }
+            console.log(data.message);
+        })
+    .catch(error=> console.error('Error', error));
+}
+
 
 
 
