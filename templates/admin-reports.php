6a72685a12e9da09eb8bece7e9c2114d398ebb34
Lamatz <lamatond444@gmail.com>
2025-05-14 19:03:36 +0800

    :changed the styling in the report css and the navbar css

--- templates/admin-reports.php
+++ templates/admin-reports.php
@@ -53,6 +53,7 @@
 
 
 
+
   <div id="layoutSidenav">
 
 
@@ -398,9 +399,9 @@
 
 
 
-<script src="../public/js/nav.js"></script>
+<!-- <script src="../public/js/nav.js"></script> -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="../public/js/generate-report.js"></script>
-
+<!-- <script src="../public/js/generate-report.js"></script> -->
+<!-- <script src="../public/js/sidebar.js"></script> -->
 
 </html>
