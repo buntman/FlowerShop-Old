2bfa74d99bd90ca714a333136b7142f13fd47301
Lamatz <lamatond444@gmail.com>
2025-05-14 19:05:26 +0800

    changed the sidebar.js code for the sidebar

--- templates/admin-reports.php
+++ templates/admin-reports.php
@@ -402,6 +402,6 @@
 <!-- <script src="../public/js/nav.js"></script> -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 <!-- <script src="../public/js/generate-report.js"></script> -->
-<!-- <script src="../public/js/sidebar.js"></script> -->
+<script src="./../public/js/sidebar.js"></script>
 
 </html>
